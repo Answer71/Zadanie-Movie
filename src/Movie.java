6a6d09244fa02dbd2f903e69c21d9992c6d09342
Answer <mazurwro@gmail.com>
2019-09-30 22:56:55 +0200

    Zrobione zadanie domowe 1

---
+++ src/Movie.java
@@ -0,0 +1,7 @@
+public class Movie {
+    String title;
+    String description;
+    String director;
+    String rating;
+    boolean oscars;
+}
---
+++ src/MovieLibrary.java
@@ -0,0 +1,18 @@
+public class MovieLibrary {
+    public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        movie1.title = "Forrest Gump";
+        movie1.description = "Historia życia Forresta, chłopca o niskim ilorazie inteligencji " +
+                "z niedowładem kończyn, który staje się miliarderem i bohaterem wojny w Wietnamie.";
+        movie1.director = "Robert Zemeckis";
+        movie1.rating = "9/10";
+        movie1.oscars = true;
+
+        System.out.println("Informacje o filmie:");
+        System.out.println("Tytuł: " + movie1.title);
+        System.out.println("Opis: " + movie1.description);
+        System.out.println("Reżyser: " + movie1.director);
+        System.out.println("Ocena: " + movie1.rating);
+        System.out.println("Czy otrzymał Oskara: " + movie1.oscars);
+    }
+}
